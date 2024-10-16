bbc1957308399c150f28539872cac343e2d0dec5
zdrobix <zdroba88@gmail.com>
2024-10-16 19:14:23 +0300

    should change player icon, the current one is too small to be seen

--- rpg_game/rpg/rpg/obj/Debug/net8.0/rpg.AssemblyInfo.cs
+++ rpg_game/rpg/rpg/obj/Debug/net8.0/rpg.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9f9550393abdd8f0bf056844259f2a37324793")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145c1d32fc9ec4421ca626110eee9e64f6d60a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("rpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
