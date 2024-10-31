88466e006f2be9aa8d98884ea84c3d157d871208
zdrobix <zdroba88@gmail.com>
2024-10-31 11:38:40 +0200

    Started assignment for 16 section. Password Generator. Changed variable names, improved visibility.
    To do:
    -unit test
    -more visbility

---
+++ passwordgen/passwordgen/passwordgen/PasswordGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passwordgen
+{
+	public class PasswordGenerator
+	{
+		public static string Generate(
+			int minLength, int maxLength, bool useSpecialCharacters)
+		{
+			if (minLength < 1)
+			{
+				throw new ArgumentOutOfRangeException(
+					$"leftRange must be greater than 0");
+			}
+			if (maxLength < minLength) { 
+				throw new ArgumentOutOfRangeException(
+					$"leftRange must be smaller than rightRange");
+			}
+
+			var lengthOfPassword = new Random().Next(minLength, maxLength + 1);
+
+			var chars = useSpecialCharacters ?
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" :
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=";
+
+			return new string(
+				Enumerable.Repeat(chars, lengthOfPassword)
+							.Select(
+									chars => 
+									chars[new Random().Next(chars.Length)])
+							.ToArray());
+		}
+	}
+}
---
+++ passwordgen/passwordgen/passwordgen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace passwordgen
+{
+	internal class Application {
+
+		public static void Main(string[] args)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine(PasswordGenerator.Generate(5, 10, false));
+			}
+			Console.ReadKey();
+		}
+	}
+}
+
+
+
+
