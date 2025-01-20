11d076a48c1cef0f78cd4b35c12e0a797000bd4e
zdrobix <zdroba88@gmail.com>
2025-01-20 17:41:32 +0200

    Started flashcard project for learning.

---
+++ flashcards/flashcards/flashcards/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace flashcards
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		private Service ServiceCards { get; set; }
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+			this.ServiceCards = new Service(new Repo());
+		}
+	}
+}
---
+++ flashcards/flashcards/flashcards/Repo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Annotations;
+using System.Windows.Media.Animation;
+
+namespace flashcards
+{
+	internal class Repo
+	{
+		private string Path = @"./input.txt";
+		private List<Card> Cards;
+
+		public Repo ()
+		{
+			Cards = new List<Card> ();
+			this.LoadFromFile();
+		}
+
+		private void LoadFromFile()
+		{
+			using (StreamReader sr = new StreamReader(this.Path))
+			{
+				string answer, question;
+				question = sr.ReadLine();
+				answer = sr.ReadLine();
+				while (answer != null && question != null)
+				{
+					this.Cards.Add(new Card(question, answer));
+					question = sr.ReadLine();
+					answer = sr.ReadLine();
+				}
+			}
+		}
+
+		public IEnumerable<Card> GetCards() => this.Cards;
+		public void RemoveCard(Card card) => this.Cards.Remove(card);
+	}
+}
